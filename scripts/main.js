b278047aa7012cc7dee8ae9c7158145126d80680
sofiaHormaza <sofiahormazasandoval@gmail.com>
2020-09-25 20:57:23 -0500

    Menu responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 //Carrousel gallery
 const galleryStripe = document.querySelector('.gallery__stripe');
 const rightArrow = document.querySelector('.gallery__arrow:last-child');
@@ -35,7 +34,6 @@ leftArrow.addEventListener('click', function () {
 })
 
 
-
 //Slider
 const slider = document.querySelector('.feature__slider');
 const img = document.querySelector('.feature__compare img:nth-child(2)');
@@ -44,3 +42,21 @@ function handleSlider() {
     img.style.width = (slider.value * 100) + '%';
 }
 slider.addEventListener('input', handleSlider);
+
+//Hamburger menu
+const open = document.querySelector('.header__burger-menu');
+const menu = document.querySelector('.burger-menu');
+const close =document.querySelector('.burger-menu__close');
+const opacity = document.querySelector('.opacity');
+
+function openHandle() {
+    menu.classList.add('burger-menu--show');
+    opacity.classList.add('opacity--show');
+}
+open.addEventListener('click', openHandle);
+
+function closeHandle() {
+    menu.classList.remove('burger-menu--show');
+    opacity.classList.remove('opacity--show');
+}
+close.addEventListener('click',closeHandle);
