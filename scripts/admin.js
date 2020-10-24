3bba8ffb01578359bb611d35cd92e56c605ae359
sofiaHormaza <sofiahormazasandoval@gmail.com>
2020-10-24 12:36:54 -0500

    agregar y eliminar productos

--- scripts/admin.js
+++ scripts/admin.js
@@ -14,5 +14,94 @@ function closeHandle() {
     modal.classList.remove('edit-add--show');
     opacity.classList.remove('opacity--show');
 }
-close.addEventListener('click',closeHandle);
+close.addEventListener('click', closeHandle);
+
+
+
+
+//BASE DE DATOS
+const db = firebase.firestore();
+const productsRef = db.collection('products');
+
+const productsList = document.querySelector('.products');
+
+function renderProducts (list) {
+    productsList.innerHTML = '';
+    list.forEach(function (elem) {
+      const newProduct = document.createElement('div');
+      newProduct.classList.add('products__glasses');
+  
+      newProduct.innerHTML = `
+      <p class="products__remove">Remove</p>
+      <div class="products__container">
+        <img class="products__imgGlasses" src="${elem.imgUrl}" alt="" >
+        <h4 class="products__title">${elem.nameProduct}</h4>
+        <p class="products__price">$${elem.price}</p>
+        <button class="button button--secondary">EDIT</button>
+      </div>
+      `;
+  
+      //Eliminar
+      const deleteBtn = newProduct.querySelector('.products__remove');
+      deleteBtn.addEventListener('click',function(){
+        productsRef.doc(elem.id).delete() .then(function() {
+          console.log("Document successfully deleted!");
+          getProducts(); 
+        })
+        .catch(function(error) {
+          console.error("Error removing document: ", error);
+        });
+      });
+  
+      //Editar
+      /*const editBtn = newProduct.querySelector('.product__edit');
+      editBtn.addEventListener('click', function() {
+        form.title.value = elem.title;
+      });*/
+  
+      productsList.appendChild(newProduct);
+    });
+  }
+
+
+  function getProducts(){
+    productsRef.get().then((querySnapshot) => {
+      const objects = [];
+      querySnapshot.forEach((doc) => {
+          const obj = doc.data();
+          obj.id = doc.id;
+          objects.push(obj);
+          console.log(`${doc.id} => ${doc.data()}`);
+      });
+      renderProducts(objects);
+    });
+  }
+
+  getProducts();
+
+
+  //Agregar producto
+const form = document.querySelector('.edit-add__form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const newProduct = {
+    nameProduct: form.nameProduct.value,
+    price: form.price.value,
+    descrip: form.descrip.value,
+    imgUrl: form.imgUrl.value,
+    shape: form.shape.value,
+    gender: form.gender.value,
+    type: form.type.value,
+  };
+
+  productsRef.add(newProduct) .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      getProducts();
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+});
+
 
---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDmAr1r7qZaIzYpBovMpn_dw3nOZFQkgg4",
+    authDomain: "web-project-e4484.firebaseapp.com",
+    databaseURL: "https://web-project-e4484.firebaseio.com",
+    projectId: "web-project-e4484",
+    storageBucket: "web-project-e4484.appspot.com",
+    messagingSenderId: "282589025269",
+    appId: "1:282589025269:web:cf046956ea6f9be5a706bc",
+    measurementId: "G-HEZC1JK3B1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
