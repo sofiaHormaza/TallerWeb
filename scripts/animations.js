30c46d545f93c0c7cbf39cd9c35e1c5c3a5d6dcd
sofiaHormaza <sofiahormazasandoval@gmail.com>
2020-12-06 16:17:07 -0500

    Animación productos de la galería

--- scripts/animations.js
+++ scripts/animations.js
@@ -8,15 +8,29 @@ gsap.registerPlugin(ScrollTrigger);
     },
 });*/
 
+//Animación textos del banner
 gsap.from(".bannerA", {
     scrollTrigger: {
       trigger: ".bannerA",
       toggleActions: "play restart restart reset",
-      start: "bottom bottom",
+      start: "top bottom",
     },
     duration: 1,
     opacity: 0,
     y: "random(-200,200)",
+    stagger: 0.25,
+  })
+
+  //Animación productos de la galería
+  gsap.from(".gallery__product", {
+    scrollTrigger: {
+      trigger: ".gallery__product",
+      toggleActions: "play restart restart reset",
+      start: "top bottom",
+    },
+    duration: 1,
+    opacity: 0,
+    y: 150,
     stagger: 0.25,
   })
 
